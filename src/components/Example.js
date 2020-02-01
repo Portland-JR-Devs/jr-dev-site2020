c6fbcd26000f1646c2b73e57c3730df526b4af95
Ben Parisot <me@benparisot.com>
2020-01-31 20:39:10 -0800

    update index and layout, write Example component

---
+++ src/components/Example.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ExampleComponent = (props) => {
+    return (
+        <>
+            <h1>An Example Page Component</h1>
+            <h2>{props.componentTitle}</h2>
+            <p>Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit​​, morbo vel maleficia? De Apocalypsi undead dictum mauris. Hi mortuis soulless creaturas, imo monstra adventus vultus comedat cerebella viventium. Qui offenderit rapto, terribilem incessu. The voodoo sacerdos suscitat mortuos comedere carnem. Search for solum oculi eorum defunctis cerebro. Nescio an Undead zombies. Sicut malus movie horror.</p>
+        </>
+    )
+}
+
+export default ExampleComponent;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import ExampleComponent from '../components/Example';
 
 const IndexPage = () => (
   <Layout>
@@ -11,6 +11,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <ExampleComponent componentTitle="About" />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
