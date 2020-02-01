53ebdd6faf177f412bf8e6c5a926d3b0bb125131
Ben Parisot <me@benparisot.com>
2020-01-31 21:16:38 -0800

    moved Example.js into example folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import ExampleComponent from '../components/Example';
+import ExampleComponent from '../components/example/Example';
 
 const IndexPage = () => (
   <Layout>
