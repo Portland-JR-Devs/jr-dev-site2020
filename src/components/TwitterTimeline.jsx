efc708b35311fe951bd461cd77f83e5073541714
jbatara <jennifer.batara.dev@gmail.com>
2020-04-25 14:24:08 -0700

    Add twitter timeline using twitter publish

---
+++ src/components/TwitterTimeline.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const TwitterTimeline = () => {
+    return (<a class="twitter-timeline" href="https://twitter.com/pdx_jr_devs?ref_src=twsrc%5Etfw">Tweets by pdx_jr_devs</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>);
+}
